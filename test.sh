77829f8e8404b8f9fa157984332199a108c7aab4
Felo Vilches <felovilches@gmail.com>
2017-06-20 16:51:03 -0400

    test bash

--- test.sh
+++ test.sh
@@ -1,19 +1,9 @@
-testcases=0; 
-for f in test/in*.txt; 
-do 
-testcases=$((testcases+1)); 
-done; 
+echo Testing...
 
-echo "testing..."
-
-
-for i in $(seq 1 $testcases)
+for f in test/in*.txt
 do
-	num=$(printf "%02g" $i)
-	echo "Test #$num"
-	diff <(./boulder.exe < test/in$num.txt) <(cat test/sol$num.txt)
+	echo Test ${f//[^0-9]/}
+	diff <(./boulder.exe < $f) <(cat ${f/in/sol})
 done
-
-
 
 echo "Ok"
